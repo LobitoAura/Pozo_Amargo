78ae2eee38c7809e126e4c0c26ec33dbc061d305
Kevin Delgado <Kevin_NDelgado11@outlook.com>
2023-02-04 19:46:22 -0300

    Add animation

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Ground : MonoBehaviour
 {
+    public float fuerzaSalto;
+
+    //private RigidBody2D rigidbody2d;
+    private Animator animator;
     // Start is called before the first frame update
-    void Start()
+    void Stard()
     {
-        
+        animator = GetComponent<Animator>();
+        //rigidbody2D = GetComponent<Rigidbody2d>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("estaSaltando", true);
+            //rigidbody2D.AddForce(new Vector2(0, fuerzaSalto));
+        }
+    }
+
+    private void OncollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            animator.SetBool("estaSaltando", false);
+        }
     }
 }
