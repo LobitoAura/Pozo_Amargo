c2c8b6988b829e7f368b162151ffeff1f37e9217
Iván Martearena <ivanofkrukoft@hotmail.com>
2023-02-05 15:13:44 -0300

    Add Boss

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    [SerializeField] private float timeDamage;
+    private float timeNextDamage;
+
+
+
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            timeNextDamage -= Time.deltaTime;
+            if (timeNextDamage <= 0)
+            {
+            other.GetComponent<PlayerHealth>().TakeDamage(20);
+            timeNextDamage = timeDamage;
+            } 
+        }
+    }
+}
